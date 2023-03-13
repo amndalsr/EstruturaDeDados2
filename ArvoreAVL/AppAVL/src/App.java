22333919d0d2ca3329545b08389e57385c4906a4
amndalsr <laisamanda2015@gmail.com>
2023-03-12 23:02:03 -0300

    initial commit

---
+++ ArvoreAVL/AppAVL/src/App.java
@@ -0,0 +1,17 @@
+import br.unicap.edii.avl.ArvoreAvl;
+import br.unicap.edii.avl.AVLNode;
+
+public class App {
+    public static void main(String[] args) {
+        ArvoreAvl tree = new ArvoreAvl();
+    
+        int[] values = {5, 2, 9, 1, 3, 7, 10};
+    
+        for (int value : values) {
+            tree.insert(value);
+        }
+    
+        System.out.println("Travessia em ordem na árvore AVL: ");
+        tree.inorder();
+    }
+}
---
+++ ArvoreAVL/avl/src/br/unicap/edii/avl/AVLNode.java
@@ -0,0 +1,11 @@
+package br.unicap.edii.avl;
+
+public class AVLNode {
+    int key, height;
+    AVLNode left, right;
+
+    AVLNode(int d) {
+        key = d;
+        height = 1;
+    }
+}
